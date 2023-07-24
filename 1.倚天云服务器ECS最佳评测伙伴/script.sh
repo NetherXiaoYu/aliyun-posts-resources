e39a68fad8ae8423c11588530649265811ae53ac
JiangXiaoYu <58361779+NetherXiaoYu@users.noreply.github.com>
2023-07-24 22:20:26 +0800

    Create script.sh
    转码测试脚本

---
+++ 1.倚天云服务器ECS最佳评测伙伴/script.sh
@@ -0,0 +1,5 @@
+apt update
+apt install -y wget ffmpeg
+wget http://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4
+mv 911Mothers_2010W-480p.mp4 test.mp4
+time ffmpeg -i test.mp4 -c:v libx265 output.mp4
