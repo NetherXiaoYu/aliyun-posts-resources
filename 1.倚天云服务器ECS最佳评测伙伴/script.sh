84c346de5ebf2aa39d8eca6006ca9f8bd248e1d5
JiangXiaoYu <58361779+NetherXiaoYu@users.noreply.github.com>
2023-07-24 23:30:01 +0800

    Update script.sh

--- 1.倚天云服务器ECS最佳评测伙伴/script.sh
+++ 1.倚天云服务器ECS最佳评测伙伴/script.sh
@@ -2,4 +2,4 @@ apt update
 apt install -y wget ffmpeg
 wget http://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4
 mv 911Mothers_2010W-480p.mp4 test.mp4
-time ffmpeg -i test.mp4 -r 60 -b 1.5M -c:v libx265 output.mp4
+time ffmpeg -i test.mp4 -c:v libx265 output.mp4
